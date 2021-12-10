78a671cff99e9e6088b81ab2de53c631b965beaf
orzzzjq <orzzzjq@gmail.com>
2021-12-10 15:34:43 +0800

    Deleted old scripts

--- run.sh
+++
@@ -1,6 +0,0 @@
-python run_train.py '../dataset/train_data_strands7_10_error0.25_seq_120.txt' '../dataset/train_label_strands7_10_error0.25_seq_120.txt' '../dataset/dev_data_strands7_10_error0.25_seq_120.txt' '../dataset/dev_label_strands7_10_error0.25_seq_120.txt' 0.0003 8
-
-
-
-
-
